d2b1bf633cca0ecea07419f46f569bd08da7d612
Julia Christ <julia_christ@outlook.com>
2021-04-22 21:55:06 -0300

    checksum ok

--- siscom.py
+++ siscom.py
@@ -3,24 +3,54 @@ import serial
 def toBinary (string):
 	return "".join(f"{ord(i):08b}" for i in string)
 
-com = serial.Serial('/dev/ttyACM0', 9600)
-com.flush()
-a = "abc"
+def toASCII_dec (string):
+	return "".join(f"{ord(i)}" for i in string)
+
+def checksum (sum_mensagem,sum_resposta):
+	if sum_mensagem == sum_resposta:
+		return 1
+	else:
+		return 0
+
+def sum_bytes(string):
+	soma = 0
+	for i in range (len(string)):
+		soma = soma + int(toASCII_dec(string[i]))
+	return soma
+
+
+USB = serial.Serial('/dev/ttyACM0', 9600)
+USB.flush()
+
 while 1 :
 	# com.write(a.encode())
 	# print (a.encode())
-	_in = input("Mensagem: ")
+	mensagem = input("Mensagem: ")
 
-	print("Entrada em bytes: ", _in.encode())
-	print("Entrada em binário: ", toBinary(_in))
-	print("N° de bytes enviados: ", com.write(_in.encode()))
+	# print(_in[1])
+	# print(toASCII_dec(_in[1]))
+
+	# print("Entrada em bytes: ", _in.encode())
+	print("Entrada em binário: ", toBinary(mensagem))
+	print("Entrada em ASCII: ", toASCII_dec(mensagem))
+	print("Soma ASCII: ", sum_bytes(mensagem))
+	print("N° de bytes enviados: ", USB.write(mensagem.encode()))
+
+	print("________________________________________\n")
+
+	resposta = USB.readline().decode('ASCII').rstrip()
+
+	print("Resposta: ", resposta)
+	print("Resposta em Binário: ", toBinary(resposta))
+	print("Soma ASCII: ", sum_bytes(resposta), "\n")
+
+	if (checksum(sum_bytes(mensagem),sum_bytes(resposta))):
+		print("Checksum OK\n")
+	else:
+		print("Checksum NOT OK\n")
+	
 
 
 
-	string = com.readline().decode('ASCII').rstrip()
-
-	print("Resposta: ",string)
-	print("Resposta em Binário: ",toBinary(string))
-
-	com.flush()
+	USB.flush()
 
