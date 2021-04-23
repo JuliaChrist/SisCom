c21029cad8eecd5078c854636cf135ed6274e19d
Julia Christ <julia_christ@outlook.com>
2021-04-22 22:28:43 -0300

    formatando soma em binário

--- siscom.py
+++ siscom.py
@@ -1,9 +1,9 @@
 import serial
 
-def toBinary (string):
+def toBinary (string): #transforma a entrada em valores bináros, sinal a ser transmitido. Codificação de linha NRZ Polar
 	return "".join(f"{ord(i):08b}" for i in string)
 
-def toASCII_dec (string):
+def toASCII_dec (string): #transforma a entrada em valores ASCII decimais correspondentes
 	return "".join(f"{ord(i)}" for i in string)
 
 def checksum (sum_mensagem,sum_resposta):
@@ -12,14 +12,16 @@ def checksum (sum_mensagem,sum_resposta):
 	else:
 		return 0
 
-def sum_bytes(string):
+def sum_bytes(string): #soma o valor dos bytes da entrada
 	soma = 0
 	for i in range (len(string)):
 		soma = soma + int(toASCII_dec(string[i]))
 	return soma
 
 
+#Configura a comunicação com um baudrate de 9600 
 USB = serial.Serial('/dev/ttyACM0', 9600)
+#Limpa o buffer
 USB.flush()
 
 while 1 :
@@ -34,23 +36,27 @@ while 1 :
 	print("Entrada em binário: ", toBinary(mensagem))
 	print("Entrada em ASCII: ", toASCII_dec(mensagem))
 	print("Soma ASCII: ", sum_bytes(mensagem))
+
+	#Envia a mensagem para a serial
 	print("N° de bytes enviados: ", USB.write(mensagem.encode()))
 
 	print("________________________________________\n")
 
+	#lê a serial
 	resposta = USB.readline().decode('ASCII').rstrip()
 
 	print("Resposta: ", resposta)
 	print("Resposta em Binário: ", toBinary(resposta))
 	print("Soma ASCII: ", sum_bytes(resposta), "\n")
 
+	#mostra o valor em binário
+	print("Soma Binário: ",format(sum_bytes(mensagem),'b'), "\n")
+
 	if (checksum(sum_bytes(mensagem),sum_bytes(resposta))):
 		print("Checksum OK\n")
 	else:
 		print("Checksum NOT OK\n")
 	
-
-
 
 	USB.flush()
 
