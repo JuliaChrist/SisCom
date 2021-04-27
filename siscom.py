d59f637f9cf964fa6720276ea525fd8032c35112
Julia Christ <julia_christ@outlook.com>
2021-04-26 21:08:27 -0300

    plotando dois graficos simultâneos

--- siscom.py
+++ siscom.py
@@ -9,6 +9,15 @@ import serial
 def toBinary (string): #transforma a entrada em valores bináros, sinal a ser transmitido. Codificação de linha NRZ Polar
 	return "".join(f"{ord(i):08b}" for i in string)
 
+def toBinaryList (string):
+	_list = []
+	str_bin = toBinary(string)
+	for i in range (len(str_bin)):
+		_list.append(int(str_bin[i]))
+
+	return _list
+
+
 def toASCII_dec (string): #transforma a entrada em valores ASCII decimais correspondentes
 	return "".join(f"{ord(i)}" for i in string)
 
@@ -18,18 +27,13 @@ def checksum (sum_mensagem,sum_resposta):
 	else:
 		return 0
 
-def sum_bytes(string): #soma o valor dos bytes da entrada
+def sum_bytes (string): #soma o valor dos bytes da entrada
 	soma = 0
 	for i in range (len(string)):
 		soma = soma + int(toASCII_dec(string[i]))
 	return soma
 
-
-
-
-
-
-def plotar(_list):
+def plotar (_list, sig):
 	prev = 0
 	graph = [] 
 	index = [0]
@@ -48,28 +52,27 @@ def plotar(_list):
 			index.append(i)
 		i +=1
 
-	plt.plot(index, graph)
-	plt.show()
+	if(sig == "msg"):
+		plt.subplot(2, 1, 1)
+		plt.title("Mensagem Enviada:")
+		plt.plot(index, graph)
+	else:
+		plt.subplot(2, 1, 2)
+		plt.title("Mensagem Recebida:")
+		plt.plot(index, graph)
 
 #Configura a comunicação com um baudrate de 9600 
 USB = serial.Serial('/dev/ttyACM0', 9600)
 #Limpa o buffer
 USB.flush()
 
-in_list = []
-
-# while 1 :
-
 #Recebendo mensagem via terminal
 mensagem = input("Mensagem: ")
-#Transformando mensagem (binária) em lista de números inteiros para plotar
-msg_bin = toBinary(mensagem)
-for i in range (len(msg_bin)):
-	in_list.append(int(msg_bin[i]))
-# print("deu? ",in_list)
-plotar(in_list)
 
-print("Entrada em binário: ",msg_bin)
+in_list = toBinaryList(mensagem)
+plotar(in_list, "msg")
+
+print("Entrada em binário: ",toBinary(mensagem))
 print("Entrada em ASCII: ", toASCII_dec(mensagem))
 print("Soma ASCII: ", sum_bytes(mensagem))
 #Envia a mensagem para a serial
@@ -79,6 +82,9 @@ print("________________________________________\n")
 
 #lê a serial
 resposta = USB.readline().decode('ASCII').rstrip()
+
+out_list = toBinaryList(resposta)
+plotar(out_list, "rsp")
 
 print("Resposta: ", resposta)
 print("Resposta em Binário: ", toBinary(resposta))
@@ -94,4 +100,6 @@ else:
 	
 
 USB.flush()
+plt.tight_layout()
+plt.show()
 
